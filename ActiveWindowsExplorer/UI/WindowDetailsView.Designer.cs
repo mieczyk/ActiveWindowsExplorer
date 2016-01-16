ea4ac0cedee7c1f79672a7a06aaaa9280bb26045
mieczyk <mieczkowski.lukasz@gmail.com>
2016-01-16 16:50:25 +0100

    WindowDetailsView user control added

---
+++ ActiveWindowsExplorer/UI/WindowDetailsView.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ActiveWindowsExplorer.UI
+{
+    partial class WindowDetailsView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._parentHandlerDecLabel = new System.Windows.Forms.Label();
+            this._handlerHexLabel = new System.Windows.Forms.Label();
+            this._classTextBox = new System.Windows.Forms.TextBox();
+            this._titleTextBox = new System.Windows.Forms.TextBox();
+            this._handlerDecLabel = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _parentHandlerDecLabel
+            // 
+            this._parentHandlerDecLabel.AutoSize = true;
+            this._parentHandlerDecLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._parentHandlerDecLabel.Location = new System.Drawing.Point(168, 80);
+            this._parentHandlerDecLabel.Name = "_parentHandlerDecLabel";
+            this._parentHandlerDecLabel.Size = new System.Drawing.Size(43, 16);
+            this._parentHandlerDecLabel.TabIndex = 20;
+            this._parentHandlerDecLabel.Text = "54646";
+            // 
+            // _handlerHexLabel
+            // 
+            this._handlerHexLabel.AutoSize = true;
+            this._handlerHexLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._handlerHexLabel.Location = new System.Drawing.Point(168, 52);
+            this._handlerHexLabel.Name = "_handlerHexLabel";
+            this._handlerHexLabel.Size = new System.Drawing.Size(73, 16);
+            this._handlerHexLabel.TabIndex = 19;
+            this._handlerHexLabel.Text = "0x34234AF";
+            // 
+            // _classTextBox
+            // 
+            this._classTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._classTextBox.Location = new System.Drawing.Point(19, 206);
+            this._classTextBox.Name = "_classTextBox";
+            this._classTextBox.Size = new System.Drawing.Size(235, 22);
+            this._classTextBox.TabIndex = 18;
+            // 
+            // _titleTextBox
+            // 
+            this._titleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._titleTextBox.Location = new System.Drawing.Point(19, 144);
+            this._titleTextBox.Name = "_titleTextBox";
+            this._titleTextBox.Size = new System.Drawing.Size(235, 22);
+            this._titleTextBox.TabIndex = 17;
+            // 
+            // _handlerDecLabel
+            // 
+            this._handlerDecLabel.AutoSize = true;
+            this._handlerDecLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._handlerDecLabel.Location = new System.Drawing.Point(168, 25);
+            this._handlerDecLabel.Name = "_handlerDecLabel";
+            this._handlerDecLabel.Size = new System.Drawing.Size(43, 16);
+            this._handlerDecLabel.TabIndex = 16;
+            this._handlerDecLabel.Text = "54646";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(16, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Class:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(16, 125);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 16);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Title:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(53, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Handler (hex):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(51, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Handler (dec):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(16, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Parent handler (dec):";
+            // 
+            // WindowDetailsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._parentHandlerDecLabel);
+            this.Controls.Add(this._handlerHexLabel);
+            this.Controls.Add(this._classTextBox);
+            this.Controls.Add(this._titleTextBox);
+            this.Controls.Add(this._handlerDecLabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "WindowDetailsView";
+            this.Size = new System.Drawing.Size(272, 255);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label _parentHandlerDecLabel;
+        private System.Windows.Forms.Label _handlerHexLabel;
+        private System.Windows.Forms.TextBox _classTextBox;
+        private System.Windows.Forms.TextBox _titleTextBox;
+        private System.Windows.Forms.Label _handlerDecLabel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
