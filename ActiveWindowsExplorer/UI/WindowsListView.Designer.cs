561ef7604ec7d3996875dcd4757a1cf3486bfc16
mieczyk <mieczkowski.lukasz@gmail.com>
2016-01-16 16:49:38 +0100

    Meaningful names of inner controls in WindowsListView

--- ActiveWindowsExplorer/UI/WindowsListView.Designer.cs
+++ ActiveWindowsExplorer/UI/WindowsListView.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WindowsListView));
             this._windowsTreeView = new System.Windows.Forms.TreeView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this._searchTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this._countLabel = new System.Windows.Forms.Label();
             this._refreshButton = new System.Windows.Forms.Button();
             this._searchButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -48,38 +48,38 @@
             this._windowsTreeView.Size = new System.Drawing.Size(500, 342);
             this._windowsTreeView.TabIndex = 2;
             // 
-            // textBox1
+            // _searchTextBox
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this._searchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(14, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(421, 22);
-            this.textBox1.TabIndex = 8;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(14, 21);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(155, 16);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Detected windows count:";
+            this._searchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._searchTextBox.Location = new System.Drawing.Point(14, 48);
+            this._searchTextBox.Name = "_searchTextBox";
+            this._searchTextBox.Size = new System.Drawing.Size(421, 22);
+            this._searchTextBox.TabIndex = 8;
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(193, 21);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(14, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(15, 16);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "0";
+            this.label1.Size = new System.Drawing.Size(155, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Detected windows count:";
+            // 
+            // _countLabel
+            // 
+            this._countLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._countLabel.AutoSize = true;
+            this._countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this._countLabel.Location = new System.Drawing.Point(193, 21);
+            this._countLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this._countLabel.Name = "_countLabel";
+            this._countLabel.Size = new System.Drawing.Size(15, 16);
+            this._countLabel.TabIndex = 11;
+            this._countLabel.Text = "0";
             // 
             // _refreshButton
             // 
@@ -109,11 +109,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this._countLabel);
             this.Controls.Add(this._refreshButton);
             this.Controls.Add(this._searchButton);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this._searchTextBox);
             this.Controls.Add(this._windowsTreeView);
             this.Name = "WindowsListView";
             this.Size = new System.Drawing.Size(526, 438);
@@ -126,9 +126,9 @@
 
         private System.Windows.Forms.TreeView _windowsTreeView;
         private System.Windows.Forms.Button _searchButton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _searchTextBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label _countLabel;
         private System.Windows.Forms.Button _refreshButton;
     }
 }
